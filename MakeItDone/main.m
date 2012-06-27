7543cbe39af13405ace805dcca332729f7f30e02
Jirat K <saakyz@gmail.com>
2012-06-27 22:43:20 +0700

    Initial Commit

---
+++ MakeItDone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MakeItDone
+//
+//  Created by Jirat K on 6/27/55 BE.
+//  Copyright (c) 2555 Allianz Global Assistance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
